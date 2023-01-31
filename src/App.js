6121f65b63a5d76a46aa9716d73af3ba9440bf9f
Sanofer Sameena <samee2914@gmail.com>
2023-01-31 17:01:40 +0530

    First Commit to create a sample form

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.scss';
+import Signin from './Components/Signin';
+
+function App() {
+  return (
+    <div>
+      <h1 className='App'>Sample Form</h1>
+      <Signin/>
+    </div>
+  );
+}
+
+export default App;
